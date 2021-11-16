e72877f64c58171eb6b74ce1380b04ba85931932
Song yunhwa <yhwasong@crossent.com>
2021-11-16 18:01:23 +0900

    socket, board 수정중

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -16,13 +16,8 @@ app.use(bodyParser()); // api 값을 parser
 app.use(router.routes()).use(router.allowedMethods());
 
 const cors = require('@koa/cors');
-// CORS 옵션
-let corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,
-};
 
-// CORS 허용
+/* CORS 허용 */
 app.proxy = true; // true 일때 proxy 헤더들을 신뢰함
 app.use(cors());
 
@@ -30,12 +25,40 @@ app.listen(port, function () {
     console.log('Example app listening on port : ' + port);
 });
 
-
 //socket 
-var io = require('socket.io')(app);
-io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-      console.log(data);
-    });
-  });
+// app은 koa module 입니다. 
+const httpServer = require('http').createServer();
+// socket.io-clinet 와 cors 설정.  
+const options = {
+cors: {
+origin: ['http://localhost:3000'],
+methods: ['GET', 'POST'],
+},
+};
+const io = require('socket.io')(httpServer, options);
+
+io.on("connection", (socket) => {      
+  console.log("connection success");
+
+  socket.on('join', (room, user) => {
+    onJoin(socket, room, user);
+});
+
+socket.on('message', (room, data) => {
+    io.sockets.in(room).emit("message", { data: data }); 
+});
+});
+
+httpServer.listen(4042);
+
+
+function onJoin(socket, room, user) {
+    console.log("Joining room: " + room);
+    socket.join(room);
+    console.log(user + "( " + socket.id + ")" + " now in " + room);
+
+    io.sockets.in(room).emit("message", { data: user + " came in." }); 
+
+}
+
+  
